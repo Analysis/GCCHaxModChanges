1605dcedc23e4f86776903e4f3ded1f17fbb279a
sohey83 <dperez97@protonmail.com>
2017-09-11 17:10:03 -0500

    A LOT of code cleanup. Add a function to map the dpad to tilt attacks.

--- HaxMod.ino
+++ HaxMod.ino
@@ -1,12 +1,3 @@
-// Record southwest and southeast notch values
-#define sw_notch_x_value -.7000
-#define sw_notch_y_value -.7000
-#define se_notch_x_value  .7000
-#define se_notch_y_value -.7000
-
-// To switch to dolphin mode hold dpad right for 5 seconds
-// To return to a stock controller hold dpad left for 10 seconds
-
 //======================================================================================================================
 //    Install Instructions:
 //
@@ -25,74 +16,37 @@
 //    6. ensure no wires are caught anywhere and close everything up
 //
 //    Note: if there is any trouble with steps 1 or 2, get the CH340 driver for your operating system from google
+//
+//    ***To switch to dolphin mode hold dpad right for 5 seconds
+//    ***To return to a stock controller hold dpad left for 10 seconds
+//
 //======================================================================================================================
 
+// Record southwest and southeast notch values
+#define sw_notch_x_value -.7000
+#define sw_notch_y_value -.7000
+#define se_notch_x_value  .7000
+#define se_notch_y_value -.7000
+
 #include <Arduino.h>
+#include <Gamecube.h>
 #include "Nintendo.h"
 
-// Sets D2 on Arduino to read data from controller
+// Sets D2 on Arduino to read data from the controller
 CGamecubeController controller(2);
 
-// Sets D3 on Arduino to write data to console
+// Sets D3 on Arduino to write data to the console
 CGamecubeConsole console(3);
 
 // Structural values for controller state
 Gamecube_Report_t gamecubeReport;
-bool shieldOn, tilt, dolphin = 0, turnCodeOff = 0;
-byte axm, aym, cxm, cym, cycles;
-char ax, ay, cx, cy, buf;
+bool shieldOn, tilt, dolphinMode, turnCodeOff;
+byte mainStickXMagnitude, mainStickYMagnitude, cStickXMagnitude, cStickYMagnitude;
+byte cycles;
+char mainStickXPos, mainStickYPos, cStickXPos, cStickYPos, buf;
 float swAngle, seAngle;
 word mode, toggle;
 unsigned long n;
-
-//======================================================================================================================
-// Reduce the dead zone in cardinal directions and give the steepest/shallowest angles when on or near the gate
-//======================================================================================================================
-void perfectAngles()
-{
-    if(axm>75)
-    {
-        gamecubeReport.xAxis = (ax>0)?204:52;
-        if(aym<23)
-        {
-            gamecubeReport.yAxis = (ay>0)?151:105;
-        }
-    }
-    if(aym>75)
-    {
-        gamecubeReport.yAxis = (ay>0)?204:52;
-        if(axm<23)
-        {
-            gamecubeReport.xAxis = (ax>0)?151:105;
-        }
-    }
-}
-//======================================================================================================================
-// Snaps sufficiently high cardinal inputs to vectors of 1.0 magnitude of analog stick and c stick
-//======================================================================================================================
-void maximizeVectors()
-{
-    if(axm>75&&aym< 9)
-    {
-        gamecubeReport.xAxis  = (ax>0)?255:1;
-        gamecubeReport.yAxis  = 128;
-    }
-    if(aym>75&&axm< 9)
-    {
-        gamecubeReport.yAxis  = (ay>0)?255:1;
-        gamecubeReport.xAxis  = 128;
-    }
-    if(cxm>75&&cym<23)
-    {
-        gamecubeReport.cxAxis = (cx>0)?255:1;
-        gamecubeReport.cyAxis = 128;
-    }
-    if(cym>75&&cxm<23)
-    {
-        gamecubeReport.cyAxis = (cy>0)?255:1;
-        gamecubeReport.cxAxis = 128;
-    }
-}
 //======================================================================================================================
 // Function to return angle in degrees when given x and y components
 //======================================================================================================================
@@ -109,27 +63,89 @@ float magnitude(char xValue, char yValue)
     return sqrt(sq(xValue)+sq(yValue));
 }
 //======================================================================================================================
+// Snaps sufficiently high cardinal inputs to vectors of 1.0 magnitude of analog stick and c stick
+//======================================================================================================================
+void maximizeVectors()
+{
+    // Maximize left and right values for the main stick
+    if(mainStickXMagnitude > 75 && mainStickYMagnitude < 9)
+    {
+        gamecubeReport.xAxis  = (mainStickXPos > 0) ? 255:1;
+        gamecubeReport.yAxis  = 128;
+    }
+    // Maximize down and up values for the main stick
+    if(mainStickYMagnitude > 75 && mainStickXMagnitude < 9)
+    {
+        gamecubeReport.yAxis  = (mainStickYPos > 0) ? 255:1;
+        gamecubeReport.xAxis  = 128;
+    }
+    // Maximize left and right values for the c-stick
+    if(cStickXMagnitude > 75 && cStickYMagnitude < 23)
+    {
+        gamecubeReport.cxAxis = (cStickXPos > 0) ? 255:1;
+        gamecubeReport.cyAxis = 128;
+    }
+    // Maximize down and up values for the c-stick
+    if(cStickYMagnitude > 75 && cStickXMagnitude < 23)
+    {
+        gamecubeReport.cyAxis = (cStickYPos > 0) ? 255:1;
+        gamecubeReport.cxAxis = 128;
+    }
+}
+//======================================================================================================================
+// Reduce the dead zone in cardinal directions and give the steepest/shallowest angles when on or near the gate
+//======================================================================================================================
+void perfectAngles()
+{
+    // TODO Give an explanation for the values (204 and 52) and (151 and 105) when you figure it out
+
+    // Determine if the user is going for steep left or right.
+    if(mainStickXMagnitude > 75)
+    {
+        // (condition) ? (value if true) : (value if false)
+        gamecubeReport.xAxis = (mainStickXPos > 0) ? 204:52;
+        if(mainStickYMagnitude < 23)
+        {
+            gamecubeReport.yAxis = (mainStickYPos > 0) ? 151:105;
+        }
+    }
+
+    // Determine if the user is going for steep up or down.
+    if(mainStickYMagnitude > 75)
+    {
+        // (condition) ? (value if true) : (value if false)
+        gamecubeReport.yAxis = (mainStickYPos > 0) ? 204:52;
+        if(mainStickXMagnitude < 23)
+        {
+            gamecubeReport.xAxis = (mainStickXPos > 0) ? 151:105;
+        }
+    }
+}
+//======================================================================================================================
 //  Allows shield drops down and gives a 6 degree range of shield dropping centered on SW and SE gates
 //======================================================================================================================
 void buffShieldDrops()
 {
-    shieldOn = (gamecubeReport.l || gamecubeReport.r || gamecubeReport.left>74 || gamecubeReport.right>74 ||gamecubeReport.z);
+    shieldOn = (gamecubeReport.l || gamecubeReport.r || gamecubeReport.left>74 || gamecubeReport.right>74 || gamecubeReport.z);
     if(shieldOn)
     {
-        if(ay<0&& magnitude(ax, ay)>75)
+        if(mainStickYPos < 0 && magnitude(mainStickXPos, mainStickYPos) > 75)
         {
 
-            if(ax<0&&abs(angle(axm, aym)-swAngle)<4)
+            if(mainStickXPos < 0 && abs(angle(mainStickXMagnitude, mainStickYMagnitude) - swAngle) < 4)
             {
-                gamecubeReport.yAxis = 73; gamecubeReport.xAxis =  73;
+                gamecubeReport.yAxis = 73;
+                gamecubeReport.xAxis = 73;
             }
 
-            if(ax>0&&abs(angle(axm, aym)-seAngle)<4)
+            if(mainStickXPos > 0 && abs(angle(mainStickXMagnitude, mainStickYMagnitude) - seAngle) < 4)
             {
-                gamecubeReport.yAxis = 73; gamecubeReport.xAxis = 183;
+                gamecubeReport.yAxis = 73;
+                gamecubeReport.xAxis = 183;
             }
         }
-        else if(abs(ay+39)<17&&axm<23) gamecubeReport.yAxis = 73;
+        else if(abs(mainStickYPos + 39) < 17 && mainStickXMagnitude < 23)
+            gamecubeReport.yAxis = 73;
     }
 }
 //======================================================================================================================
@@ -137,22 +153,59 @@ void buffShieldDrops()
 //======================================================================================================================
 void fixDashBack()
 {
-    if(aym<23)
+    if(mainStickYMagnitude<23)
     {
-        if(axm<23)
+        if(mainStickXMagnitude<23)
         {
             buf = cycles;
         }
         if(buf>0)
         {
             buf--;
-            if(axm<64)
+            if(mainStickXMagnitude<64)
             {
-                gamecubeReport.xAxis = 128+ax*(axm<23);
+                gamecubeReport.xAxis = 128+mainStickXPos*(mainStickXMagnitude<23);
             }
         }
     }
     else buf = 0;
+}
+//======================================================================================================================
+// This function will map the dpad to tilt attacks
+//======================================================================================================================
+void dpadTilts()
+{
+    gamecubeReport.dup = false;
+    if(gamecubeReport.dup && gamecubeReport.dright)
+    {
+        gamecubeReport.xAxis = 105;
+        gamecubeReport.a = true;
+    }
+    else if(gamecubeReport.dup && gamecubeReport.dleft)
+    {
+        gamecubeReport.xAxis = 152;
+        gamecubeReport.a = true;
+    }
+    else if(gamecubeReport.dright)
+    {
+        gamecubeReport.xAxis = 255;
+        gamecubeReport.a = true;
+    }
+    else if(gamecubeReport.dleft)
+    {
+        gamecubeReport.xAxis = 1;
+        gamecubeReport.a = true;
+    }
+    else if(gamecubeReport.dup)
+    {
+        gamecubeReport.yAxis = 255;
+        gamecubeReport.a = true;
+    }
+    else if(gamecubeReport.ddown)
+    {
+        gamecubeReport.yAxis = 1;
+        gamecubeReport.a = true;
+    }
 }
 //======================================================================================================================
 // Ensures close to 0 values are reported as 0 on the sticks to fix dolphin calibration and allows user to switch to
@@ -160,21 +213,23 @@ void fixDashBack()
 //======================================================================================================================
 void dolphinFix()
 {
-    if(axm<5&&aym<5)
+    if(mainStickXMagnitude < 5 && mainStickYMagnitude < 5)
     {
-        gamecubeReport.xAxis  = 128; gamecubeReport.yAxis  = 128;
+        gamecubeReport.xAxis  = 128;
+        gamecubeReport.yAxis  = 128;
     }
-    if(cxm<5&&cym<5)
+    if(cStickXMagnitude<5&&cStickYMagnitude<5)
     {
-        gamecubeReport.cxAxis = 128; gamecubeReport.cyAxis = 128;
+        gamecubeReport.cxAxis = 128;
+        gamecubeReport.cyAxis = 128;
     }
-    if(gamecubeReport.dright && mode<2500)
+    if(gamecubeReport.dright && mode < 2500)
     {
-        dolphin = dolphin||(mode++>2000);
+        dolphinMode = dolphinMode || (mode++ > 2000);
     }
     else mode = 0;
 
-    cycles = 3 + (6*dolphin);
+    cycles = 3 + (6*dolphinMode);
 }
 //======================================================================================================================
 // This function will turn off all code is the LEFT on the dpad is held for 10 seconds
@@ -189,7 +244,7 @@ void determineCodeOff()
             n = millis();
         }
 
-        turnCodeOff = turnCodeOff||(millis()-n>2000);
+        turnCodeOff = turnCodeOff || (millis()-n>2000);
 
     }
     else n = 0;
@@ -211,6 +266,9 @@ void convertInputs()
     // Fixes dashback by imposing a 1 frame buffer upon tilt turn values
     fixDashBack();
 
+    // Maps tilts to the DPAD
+    dpadTilts();
+
     // Ensures close to 0 values are reported as 0 on the sticks to fix dolphin calibration and allows user to switch to dolphin mode for backdash
     dolphinFix();
 
@@ -228,11 +286,15 @@ void setup()
     gamecubeReport.high1   = 0;
     gamecubeReport.errstat = 0;
 
+    // Set up
+    dolphinMode = false;
+    turnCodeOff = false;
+
     // Calculates angle of SW gate base on user inputted data
-    swAngle = angle(abs(sw_notch_x_value), abs(sw_notch_y_value));
+    swAngle = angle( abs(sw_notch_x_value), abs(sw_notch_y_value) );
 
     // Calculates angle of SE gate based on user inputted data
-    seAngle = angle(abs(se_notch_x_value), abs(se_notch_y_value));
+    seAngle = angle( abs(se_notch_x_value), abs(se_notch_y_value) );
 }
 //======================================================================================================================
 // Main loop for the program to run
@@ -243,21 +305,21 @@ void loop()
     controller.read();
     gamecubeReport = controller.getReport();
 
-    // Offsets from neutral position of analog stick
-    ax = gamecubeReport.xAxis -128;
-    ay = gamecubeReport.yAxis -128;
+    // Determine the position of the main analog stick from the center point.
+    mainStickXPos = gamecubeReport.xAxis - 128;
+    mainStickYPos = gamecubeReport.yAxis - 128;
 
-    // Offsets from neutral position of c stick
-    cx = gamecubeReport.cxAxis-128;
-    cy = gamecubeReport.cyAxis-128;
+    // Determine the position of the C-Stick from the center point.
+    cStickXPos = gamecubeReport.cxAxis - 128;
+    cStickYPos = gamecubeReport.cyAxis - 128;
 
     // Magnitude of analog stick offsets
-    axm = abs(ax);
-    aym = abs(ay);
+    mainStickXMagnitude = abs(mainStickXPos);
+    mainStickYMagnitude = abs(mainStickYPos);
 
     // Magnitude of c stick offsets
-    cxm = abs(cx);
-    cym = abs(cy);
+    cStickXMagnitude = abs(cStickXPos);
+    cStickYMagnitude = abs(cStickYPos);
 
     // Determine if the user wants the code off
     if(!turnCodeOff)
